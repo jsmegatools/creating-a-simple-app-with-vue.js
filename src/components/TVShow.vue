09c9989a1929a7b32862866cca229c25afac7dc2
Sergey Smolin <soees84@gmail.com>
2017-11-27 21:51:46 +0500

    Add TVShow component page

--- src/components/TVShow.vue
+++ src/components/TVShow.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="tv_show">
-    TV show
+    <div>{{ show.name }}</div>
+    <img v-bind:src="`static/${show.image}`" />
+    <h1>Reviews</h1>
+    <ul class="show_reviews">
+      <li class="show_review" v-for="review in show.reviews" :key="review.id">
+        <span>{{ review.reviewer }}</span>
+        <p>{{ review.text }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import TVShows from '../mock_data.js'
+
 export default {
-  name: 'TVShow'
+  name: 'TVShow',
+  created: function () {
+    setTimeout(() => {
+      this.show = TVShows.find(show => show.id === this.$route.params.id)
+    })
+  },
+  data: function () {
+    return {
+      show: {}
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .show_reviews {
+    list-style: none;
+    text-align: left;
+  }
+  .show_review {
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    margin-bottom: 5px;
+    padding: 5px;
+  }
 </style>
--- src/mock_data.js
+++ src/mock_data.js
@@ -2,16 +2,67 @@ export default [
   {
     id: '1',
     name: 'Physical Worker',
-    image: 'vintage-1133810_640.jpg'
+    image: 'vintage-1133810_640.jpg',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Barbora Mcsheehy',
+        text: 'Vivamus interdum ipsum orci, id commodo nibh tincidunt eu. Quisque et posuere arcu. Sed non felis sit amet nibh auctor gravida non et magna. Curabitur id maximus mauris, nec semper neque. Pellentesque congue porttitor tincidunt. Proin facilisis tincidunt tellus, vitae suscipit lacus consectetur egestas. In mollis orci eget lorem cursus blandit.'
+      },
+      {
+        id: 2,
+        reviewer: 'Marco Bateson',
+        text: 'Ut ligula augue, semper ut pulvinar non, lobortis ut nisl. Aenean a augue in enim blandit ultrices sodales sed erat. Aenean id tempus risus. Pellentesque cursus sollicitudin congue. Praesent pharetra mauris at lacus rhoncus hendrerit. Nulla lobortis sagittis ex eu gravida. Etiam pharetra semper dolor, quis varius ex elementum a. Integer condimentum ante ac mauris elementum laoreet at eu diam.'
+      },
+      {
+        id: 3,
+        reviewer: 'Ceinwen Das',
+        text: 'Vestibulum aliquam tortor ut ligula elementum, vel imperdiet mi elementum. Nullam porta mi id orci venenatis, vel maximus orci porttitor. Suspendisse potenti. Cras rhoncus libero lectus, id aliquet dui hendrerit id. Morbi tincidunt accumsan elit vitae rhoncus. Nulla facilisi. Fusce mattis nisi consequat orci laoreet, vel facilisis tortor convallis. Aliquam posuere mauris turpis, eget vestibulum lectus tincidunt nec.'
+      }
+    ]
   },
   {
     id: '2',
     name: 'Hard Face',
-    image: 'white-male-1847732_640.jpg'
+    image: 'white-male-1847732_640.jpg',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Gianna Barber',
+        text: 'Curabitur ut magna nibh. Praesent viverra ligula ac lorem auctor congue. Aenean urna ante, dictum et posuere vitae, bibendum id nunc. Nullam at tortor ac lectus egestas consequat ut sit amet diam. Nunc consectetur tortor euismod, congue purus nec, tristique nibh. Phasellus vitae tellus quis eros tempus fringilla. Pellentesque lobortis ante eu mattis aliquam. '
+      },
+      {
+        id: 2,
+        reviewer: 'Barbora Mcsheehy',
+        text: 'Vivamus ac hendrerit ligula. Cras egestas cursus viverra. Proin et libero lobortis, aliquam dui eu, pellentesque nisi. Phasellus vitae ligula quis tortor ultricies vestibulum id at nunc. Nunc sit amet augue in nisl semper viverra.'
+      },
+      {
+        id: 3,
+        reviewer: 'Malina Zaman',
+        text: 'Morbi est leo, finibus eget lacinia quis, elementum nec tortor. Aliquam iaculis accumsan ipsum, a blandit mi rutrum ut. Sed tempus ultricies bibendum. Integer elementum consectetur libero, sit amet tincidunt dui.'
+      }
+    ]
   },
   {
     id: '3',
     name: 'Changing Catalog',
-    image: 'white-male-1871447_640.jpg'
+    image: 'white-male-1871447_640.jpg',
+    reviews: [
+      {
+        id: 1,
+        reviewer: 'Hildegard Houtman',
+        text: 'Praesent purus urna, tincidunt id faucibus in, tincidunt nec felis. Nunc diam tortor, sodales eu quam eget, cursus molestie magna. Proin eu felis eget enim ornare facilisis vel nec orci. Phasellus bibendum, enim et auctor bibendum, sem eros cursus lacus, sit amet finibus urna felis a leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+      },
+      {
+        id: 2,
+        reviewer: 'Marco Bateson',
+        text: 'Sed malesuada massa nec odio commodo molestie. Nulla facilisi. Vestibulum ultricies, eros a interdum pellentesque, lacus justo vulputate augue, in convallis augue diam ut dolor.'
+      },
+      {
+        id: 3,
+        reviewer: 'Ekain Hagopian',
+        text: 'Pellentesque rutrum lectus vitae ante aliquet, eget mollis metus aliquet. Ut scelerisque faucibus justo et malesuada. Maecenas efficitur est eget varius porta. Integer rhoncus neque eu suscipit dictum. Nulla at volutpat quam. Vivamus tincidunt vel nulla et consectetur. Fusce a diam vitae quam pulvinar viverra.'
+      }
+    ]
   }
 ]
